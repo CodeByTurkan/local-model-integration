72ce0394e535edb2e5c022ff2482aa6d977c182d
Turkan Isayeva <turkantech@gmail.com>
2026-04-06 10:08:12 +0400

    speech recognation  creation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { pipeline } from "@Xenova/transformers";
-
+import wavefile from 'wavefile';
 // const classifier = await pipeline("sentiment-analysis");
 
 async function embedder() {
@@ -26,4 +26,34 @@ async function textGeneration() {
   });
   console.log(response);
 }
-textGeneration();
+// textGeneration();
+
+
+async function speechRecognition() {
+    let transcriber = await pipeline('automatic-speech-recognition', 'Xenova/whisper-small.en');
+    let url = 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/jfk.wav';
+    let buffer = Buffer.from(await fetch(url).then(x => x.arrayBuffer()))
+
+    let wav = new wavefile.WaveFile(buffer);
+    wav.toBitDepth('32f'); // Pipeline expects input as a Float32Array
+    wav.toSampleRate(16000); // Whisper expects audio with a sampling rate of 16000
+
+    let audioData = wav.getSamples();
+    if (Array.isArray(audioData)) {
+        if (audioData.length > 1) {
+            const SCALING_FACTOR = Math.sqrt(2);
+
+            // Merge channels (into first channel to save memory)
+            for (let i = 0; i < audioData[0].length; ++i) {
+                audioData[0][i] = SCALING_FACTOR * (audioData[0][i] + audioData[1][i]) / 2;
+            }
+        }
+
+            // Select first channel
+            audioData = audioData[0];
+        }
+    
+        const response = await transcriber(audioData);
+        console.log(response);
+    }
+    speechRecognition()
